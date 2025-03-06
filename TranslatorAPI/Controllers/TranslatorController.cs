90c71f1e91994e4ee1f41d7272b31b87936a4045
Felix Cadieux <cadieuxfelix@outlook.com>
2025-03-05 21:45:53 -0500

    Rest api works

--- TranslatorAPI/Controllers/TranslatorController.cs
+++ TranslatorAPI/Controllers/TranslatorController.cs
@@ -5,7 +5,7 @@ using TranslatorAPI.Data;
 
 namespace TranslatorAPI.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]/[action]")]
     [ApiController]
     public class TranslatorController : ControllerBase
     {
@@ -14,14 +14,16 @@ namespace TranslatorAPI.Controllers
         {
             _context = context;
         }
+
+        // Post or update
         [HttpPost]
         public JsonResult CreateEdit(User user)
         {
-            if (user.Id == null)
+            if (user.Id == 0)
             {
                 _context.Users.Add(user);
                 _context.SaveChanges();
-                return new JsonResult("User created successfully");
+                return new JsonResult(new { message = "User created successfully", user });
             }
             else
             {
@@ -30,11 +32,47 @@ namespace TranslatorAPI.Controllers
                 {
                     return new JsonResult(NotFound());
                 }
-                existingUser = user;
-              
+                existingUser.Name = user.Name;
+                existingUser.Email = user.Email;
+                existingUser.Password = user.Password;
+                
+                _context.SaveChanges();
             }
-            _context.SaveChanges();
             return new JsonResult(Ok(user));
+        }
+
+        // Get a single user
+        [HttpGet]
+        public JsonResult Get(int id)
+        {
+            var result = _context.Users.Find(id);
+
+            if (result == null)
+                return new JsonResult(NotFound());
+
+            return new JsonResult(Ok(result));
+        }
+
+        // Delete
+        [HttpDelete]
+        public JsonResult Delete(int id)
+        {
+            var result = _context.Users.Find(id);
+
+            if (result == null)
+                return new JsonResult(NotFound());
+
+            _context.Users.Remove(result);
+            _context.SaveChanges();
+
+            return new JsonResult(NoContent());
+        }
+
+        [HttpGet()]
+        public JsonResult GetAll()
+        {
+            var result = _context.Users.ToList();
+            return new JsonResult(Ok(result));
         }
     }
 }
